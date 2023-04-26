cb9700c0f68cb2c4653cd5f2bd7dd7d9ba01fb8d
Slender357 <114290259+Slender357@users.noreply.github.com>
2023-04-26 20:55:33 +0300

    add annotation

--- src/local/api/v1/anotation.py
+++ src/local/api/v1/anotation.py
@@ -3,8 +3,11 @@ PAGINATION_PAGE = "Страница выдачи"
 GENRE_ID = "UUID Жанра"
 PERSON_ID = "UUID Персоны"
 PERSON_QUERY = "Запрос на поиск"
+<<<<<<< HEAD
 SORTING = "Сортировка"
 FILMS_QUERY = "Запрос на поиск фильмов"
 FILM_ID = "UUID Фильма"
 FILMS_NOT_FOUND = "Фильмы не найдены"
 FILM_NOT_FOUND = "Фильм не найден"
+=======
+>>>>>>> add annotation
