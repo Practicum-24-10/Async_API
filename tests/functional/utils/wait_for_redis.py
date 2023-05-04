3530a0150879d37cc4ef3bc4094724b64e93f688
Slender357 <114290259+Slender357@users.noreply.github.com>
2023-05-04 16:20:36 +0300

    add test infra

---
+++ tests/functional/utils/wait_for_redis.py
@@ -0,0 +1,15 @@
+import time
+
+from redis import Redis
+
+from tests.functional.utils.settings_wait_for import RedisSettings
+
+if __name__ == '__main__':
+    config = RedisSettings()
+    red_client = Redis(
+        host=config.redis_host, port=config.redis_port
+    )
+    while True:
+        if red_client.ping():
+            break
+        time.sleep(1)
