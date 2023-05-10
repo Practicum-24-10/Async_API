74f60bc5ced2689e2b1d3e01662c9ca97fe70cde
tyshbyr <tyshbyr@gmail.com>
2023-05-10 17:26:22 +0500

    Add delete es index

--- tests/functional/src/test_film_search.py
+++ tests/functional/src/test_film_search.py
@@ -7,19 +7,22 @@ from tests.functional.testdata.es_data import films_data
     'query_data, expected_answer',
     [
         (
-            {'query': 'The Star', 'page_size': '60', 'page_number': '1'},
+            {'query': 'The Star', 'size': '60', 'page': '1'},
             {'status': 200, 'length': 60}
         ),
         (
-            {'query': 'Mashed potato', 'page_size': '60', 'page_number': '1'},
+            {'query': 'Mashed potato', 'size': '60', 'page': '1'},
             {'status': 404, 'length': 1}
         )
     ]
 )
 @pytest.mark.asyncio
-async def test_search(make_get_request, es_write_data, query_data,
+async def test_search(es_delete_data, make_get_request, es_write_data, query_data,
                       expected_answer):
     await es_write_data(films_data)
     response = await make_get_request('films/search', query_data)
+    assert response['status'] == expected_answer['status']
+    assert len(response['body']) == expected_answer['length']
+    await es_delete_data()
     assert response['status'] == expected_answer['status']
     assert len(response['body']) == expected_answer['length']
