34222ba7552c4160e6a1f23050697d818ec56db6
tyshbyr <tyshbyr@gmail.com>
2023-05-12 01:36:29 +0500

    Update films search

--- tests/functional/src/test_films_search.py
+++ tests/functional/src/test_films_search.py
@@ -1,5 +1,9 @@
 import pytest
 
+from tests.functional.testdata.es_data import film
+
+film = film[0]
+
 
 @pytest.mark.parametrize(
     'query_data, expected_answer',
@@ -9,19 +13,62 @@ import pytest
             {'status': 200, 'length': 60}
         ),
         (
+            {'query': 'The Moon', 'size': '60', 'page': '1'},
+            {'status': 200, 'length': 1}
+        ),
+        (
             {'query': 'Mashed potato', 'size': '60', 'page': '1'},
             {'status': 404, 'length': 1}
-        )
+        ),
+        (
+            {'query': 'The Star', 'size': '1', 'page': '-1'},
+            {'status': 422, 'length': 1}
+        ),
+        (
+            {'query': 'The Star', 'size': '-1', 'page': '1'},
+            {'status': 422, 'length': 1}
+        ),
+        (
+            {'query': 'The Star', 'size': '0', 'page': '1'},
+            {'status': 422, 'length': 1}
+        ),
+        (
+            {'query': 'The Star', 'size': '1', 'page': '0'},
+            {'status': 422, 'length': 1}
+        ),
     ]
 )
 @pytest.mark.asyncio
 async def test_film_search(es_delete_data, make_get_request, es_write_data,
                            query_data, expected_answer):
+    async def run_assert():
+        response = await make_get_request('films/search', query_data)
+        assert response['status'] == expected_answer['status']
+        assert len(response['body']) == expected_answer['length']
     await es_write_data()
-    response = await make_get_request('films/search', query_data)
-    assert response['status'] == expected_answer['status']
-    assert len(response['body']) == expected_answer['length']
+    await run_assert()
     await es_delete_data()
-    response = await make_get_request('films/search', query_data)
-    assert response['status'] == expected_answer['status']
-    assert len(response['body']) == expected_answer['length']
+    await run_assert()
+
+
+@pytest.mark.parametrize(
+    'query_data, expected_answer',
+    [
+        (
+            {'query': 'The Moon', 'size': '60', 'page': '1'},
+            {'id': film['id'], 'title': film['title'], 'imdb_rating': film['imdb_rating']}
+        )
+    ]
+)
+@pytest.mark.asyncio
+async def test_film_search_check_content(
+        es_delete_data, make_get_request, es_write_data, query_data, expected_answer):
+    async def run_assert():
+        response = await make_get_request('films/search', query_data)
+        assert response['body'][0]['uuid'] == expected_answer['id']
+        assert response['body'][0]['title'] == expected_answer['title']
+        assert response['body'][0]['imdb_rating'] == expected_answer['imdb_rating']
+    await es_write_data()
+    await run_assert()
+    await es_delete_data()
+    await run_assert()
